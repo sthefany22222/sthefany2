9d50057e78010f0441a4e0085dc69027236c9dd1
sthefany22222 <118447283+sthefany22222@users.noreply.github.com>
2023-10-19 09:01:59 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,17 @@
+//pom------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom
+const listaDeTeclas = document.querySelectorAll('.tecla') 
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas [contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
