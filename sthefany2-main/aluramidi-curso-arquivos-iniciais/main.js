851070bdb1b9ff7c77d563aa41bd6689682f1f71
sthefany22222 <118447283+sthefany22222@users.noreply.github.com>
2023-11-09 09:03:24 -0300

    Add files via upload

--- sthefany2-main/aluramidi-curso-arquivos-iniciais/main.js
+++ sthefany2-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,3 +1,24 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
-document.querySelector('#som_tecla_pom').play();
+const listaDeTeclas = document.querySelectorAll('.tecla') 
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template  string
+    const idAudio = `#som_${instrumento}`
+    console.log(idAudio);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
